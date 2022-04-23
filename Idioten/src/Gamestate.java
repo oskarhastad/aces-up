e865bfe353461eeb9ef320b17b081d28e9ef0b74
oskarhastad <oskar.hastad@gmail.com>
2022-04-23 21:45:44 +0200

    Implemented moveCard with a very simple trial algorithm. Also added amountOfCards and cloneGamestate needed for planned implementation of a more advanced algorithm.

--- Idioten/src/Gamestate.java
+++ Idioten/src/Gamestate.java
@@ -17,12 +17,18 @@ public class Gamestate {
 			pile.add(deck.drawRandomCard());
 		
 	}
+	
+	
+	void removeAndMove() {
+		
+		
+		
+	}
 
 	void compareAndRemove() {	
 		boolean changed = true;
 		while (changed) {
 			changed = false;
-			System.out.println("Checking once");
 			for(int i = 0; i < piles.size();i++ ) {
 				for (int j = i+1; j < piles.size(); j++ ) {
 					if(!piles.get(i).isEmpty() && !piles.get(j).isEmpty()) {
@@ -34,9 +40,9 @@ public class Gamestate {
 			}
 		}
 	}
-	 boolean compareAndRemoveLast(List<Card> firstPile, List<Card> secondPile) {
-		 Card lastPileOne = firstPile.get(firstPile.size()-1);
-		 Card lastPileTwo = secondPile.get(secondPile.size()-1);
+	 boolean compareAndRemoveLast(LinkedList<Card> firstPile, LinkedList<Card> secondPile) {
+		 Card lastPileOne = firstPile.getLast();
+		 Card lastPileTwo = secondPile.getLast();
 		 if(lastPileOne.suit == lastPileTwo.suit){
 			 if(lastPileOne.value > lastPileTwo.value) {
 				 secondPile.remove(secondPile.size()-1);
@@ -50,6 +56,57 @@ public class Gamestate {
 	 }
 	 
 	 
+	 void moveCards() {
+		 LinkedList<LinkedList<Card>> empty = new LinkedList<LinkedList<Card>>();
+		 LinkedList<LinkedList<Card>> canMove = new LinkedList<LinkedList<Card>>();
+		 for (int i = 0; i < piles.size(); i++) {
+			 if(piles.get(i).isEmpty()) {
+				 empty.add(piles.get(i));
+			 }
+			 if(piles.get(i).size() >= 2) {
+				 canMove.add(piles.get(i));
+			 }
+		 }
+		 
+		 if(!empty.isEmpty() && !canMove.isEmpty()) {
+			 for (LinkedList<Card> pile : empty) {
+				 if(!canMove.isEmpty()) {
+					 pile.add(canMove.getLast().getLast());
+					 canMove.getLast().removeLast();
+					 if(canMove.getLast().size() < 2) {
+						 canMove.removeLast();
+					 }
+				 }
+			 }
+
+		 }
+	}
+	 
+	 
+	 
+	 int amountOfCards() {
+		 int amount = 0;
+		 for (LinkedList<Card> pile : piles) {
+			 amount = amount + pile.size();
+		 }
+		 return amount;
+	 }
+	 
+	 
+	 	Gamestate cloneGamestate(){
+	 		List<LinkedList<Card>> copy = new ArrayList<LinkedList<Card>>();
+	 		for(int i = 0; i < piles.size(); i++) {
+	 			LinkedList<Card> tempList = new LinkedList<Card>();
+	 			copy.add(tempList);
+	 			for(int j = 0; j < piles.get(i).size(); j++) {
+	 				Card tempcard = new Card(piles.get(i).get(j).value, piles.get(i).get(j).suit);
+	 				copy.get(i).add(tempcard);
+	 			}
+	 		}
+		 return new Gamestate(copy.get(0),copy.get(1),copy.get(2),copy.get(3));
+	 }
+	 
+	 
 	 void printGamestate() {
 		 for(LinkedList<Card> pile : piles) {
 			 System.out.println("-----------------------");
@@ -58,6 +115,5 @@ public class Gamestate {
 			 }
 		 }
 	 }
-	
 }
 
