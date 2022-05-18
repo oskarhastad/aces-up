776ec3038c0d35c7431ff323b1b721881d7c8a47
oskarhastad <oskar.hastad@gmail.com>
2022-05-18 14:57:35 +0200

    Added smarter algorithm for moving cards, simulating success rate with 1000 runs

--- Idioten/src/Gamestate.java
+++ Idioten/src/Gamestate.java
@@ -1,119 +1,142 @@
 import java.util.*;
 
 public class Gamestate {
-	
-	
+
 	List<LinkedList<Card>> piles = new ArrayList<LinkedList<Card>>();
-	
-	Gamestate(LinkedList<Card> pileOne, LinkedList<Card> pileTwo, LinkedList<Card> pileThree, LinkedList<Card> pileFour) {
+	LinkedList<LinkedList<Card>> empty = new LinkedList<LinkedList<Card>>();
+	LinkedList<LinkedList<Card>> canMove = new LinkedList<LinkedList<Card>>();
+
+	Gamestate(LinkedList<Card> pileOne, LinkedList<Card> pileTwo, LinkedList<Card> pileThree,
+			LinkedList<Card> pileFour) {
 		piles.add(pileOne);
 		piles.add(pileTwo);
 		piles.add(pileThree);
 		piles.add(pileFour);
-	
-	}
-	void dealCards(Deck deck) {
-		for(LinkedList<Card> pile : piles)
-			pile.add(deck.drawRandomCard());
-		
-	}
-	
-	
-	void removeAndMove() {
-		
-		
-		
+
 	}
 
-	void compareAndRemove() {	
+	void dealCards(Deck deck) {
+		for (LinkedList<Card> pile : piles)
+			pile.add(deck.drawRandomCard());
+
+	}
+
+	void removeAndMove() {
+
+	}
+
+	void compareAndRemove() {
 		boolean changed = true;
 		while (changed) {
 			changed = false;
-			for(int i = 0; i < piles.size();i++ ) {
-				for (int j = i+1; j < piles.size(); j++ ) {
-					if(!piles.get(i).isEmpty() && !piles.get(j).isEmpty()) {
-						if (compareAndRemoveLast(piles.get(i),piles.get(j))) {
-						changed = true;
+			for (int i = 0; i < piles.size(); i++) {
+				for (int j = i + 1; j < piles.size(); j++) {
+					if (!piles.get(i).isEmpty() && !piles.get(j).isEmpty()) {
+						if (compareAndRemoveLast(piles.get(i), piles.get(j))) {
+							changed = true;
 						}
 					}
 				}
 			}
 		}
 	}
-	 boolean compareAndRemoveLast(LinkedList<Card> firstPile, LinkedList<Card> secondPile) {
-		 Card lastPileOne = firstPile.getLast();
-		 Card lastPileTwo = secondPile.getLast();
-		 if(lastPileOne.suit == lastPileTwo.suit){
-			 if(lastPileOne.value > lastPileTwo.value) {
-				 secondPile.remove(secondPile.size()-1);
-				 return true;
-			 } else {
-				 firstPile.remove(firstPile.size()-1);
-				 return true;
-			 }
-		 }
-		 return false;
-	 }
-	 
-	 
-	 void moveCards() {
-		 LinkedList<LinkedList<Card>> empty = new LinkedList<LinkedList<Card>>();
-		 LinkedList<LinkedList<Card>> canMove = new LinkedList<LinkedList<Card>>();
-		 for (int i = 0; i < piles.size(); i++) {
-			 if(piles.get(i).isEmpty()) {
-				 empty.add(piles.get(i));
-			 }
-			 if(piles.get(i).size() >= 2) {
-				 canMove.add(piles.get(i));
-			 }
-		 }
-		 
-		 if(!empty.isEmpty() && !canMove.isEmpty()) {
-			 for (LinkedList<Card> pile : empty) {
-				 if(!canMove.isEmpty()) {
-					 pile.add(canMove.getLast().getLast());
-					 canMove.getLast().removeLast();
-					 if(canMove.getLast().size() < 2) {
-						 canMove.removeLast();
-					 }
-				 }
-			 }
 
-		 }
+	boolean compareAndRemoveLast(LinkedList<Card> firstPile, LinkedList<Card> secondPile) {
+		Card lastPileOne = firstPile.getLast();
+		Card lastPileTwo = secondPile.getLast();
+		if (lastPileOne.suit == lastPileTwo.suit) {
+			if (lastPileOne.value > lastPileTwo.value) {
+				secondPile.remove(secondPile.size() - 1);
+				return true;
+			} else {
+				firstPile.remove(firstPile.size() - 1);
+				return true;
+			}
+		}
+		return false;
 	}
-	 
-	 
-	 
-	 int amountOfCards() {
-		 int amount = 0;
-		 for (LinkedList<Card> pile : piles) {
-			 amount = amount + pile.size();
-		 }
-		 return amount;
-	 }
-	 
-	 
-	 	Gamestate cloneGamestate(){
-	 		List<LinkedList<Card>> copy = new ArrayList<LinkedList<Card>>();
-	 		for(int i = 0; i < piles.size(); i++) {
-	 			LinkedList<Card> tempList = new LinkedList<Card>();
-	 			copy.add(tempList);
-	 			for(int j = 0; j < piles.get(i).size(); j++) {
-	 				Card tempcard = new Card(piles.get(i).get(j).value, piles.get(i).get(j).suit);
-	 				copy.get(i).add(tempcard);
-	 			}
-	 		}
-		 return new Gamestate(copy.get(0),copy.get(1),copy.get(2),copy.get(3));
-	 }
-	 
-	 
-	 void printGamestate() {
-		 for(LinkedList<Card> pile : piles) {
-			 System.out.println("-----------------------");
-			 for(Card c : pile) {
-				 c.printCard();
-			 }
-		 }
-	 }
-}
 
+	void moveCards() {
+		fillEmptyAndCanMove();
+		if (!empty.isEmpty() && !canMove.isEmpty()) {
+			for (LinkedList<Card> moveTo : empty) {
+				Gamestate candidate = cloneGamestate();
+				candidate.fillEmptyAndCanMove();
+				for (LinkedList<Card> moveFrom : candidate.canMove) {
+					Gamestate candidateTemp = cloneGamestate();
+					candidateTemp.fillEmptyAndCanMove();
+					moveTo.add(moveFrom.getLast());
+					moveFrom.removeLast();
+					candidateTemp.compareAndRemove();
+					if (candidateTemp.amountOfCards() <= candidate.amountOfCards()) {
+						candidate = candidateTemp;
+					}
+				}
+				piles = candidate.piles;
+				candidate.fillEmptyAndCanMove();
+			}
+		}
+
+	}
+
+	void moveCardsRandom() {
+		fillEmptyAndCanMove();
+		if (!empty.isEmpty() && !canMove.isEmpty()) {
+			for (LinkedList<Card> pile : empty) {
+				if (!canMove.isEmpty()) {
+					pile.add(canMove.getLast().getLast());
+					canMove.getLast().removeLast();
+					if (canMove.getLast().size() < 2) {
+						canMove.removeLast();
+					}
+				}
+			}
+
+		}
+
+	}
+
+	void fillEmptyAndCanMove() {
+		empty.clear();
+		canMove.clear();
+		for (LinkedList<Card> checkPile : piles) {
+			if (checkPile.isEmpty()) {
+				empty.add(checkPile);
+			}
+			if (checkPile.size() >= 2) {
+				canMove.add(checkPile);
+			}
+		}
+
+	}
+
+	int amountOfCards() {
+		int amount = 0;
+		for (LinkedList<Card> pile : piles) {
+			amount = amount + pile.size();
+		}
+		return amount;
+	}
+
+	Gamestate cloneGamestate() {
+		List<LinkedList<Card>> copy = new ArrayList<LinkedList<Card>>();
+		for (int i = 0; i < piles.size(); i++) {
+			LinkedList<Card> tempList = new LinkedList<Card>();
+			copy.add(tempList);
+			for (int j = 0; j < piles.get(i).size(); j++) {
+				Card tempcard = new Card(piles.get(i).get(j).value, piles.get(i).get(j).suit);
+				copy.get(i).add(tempcard);
+			}
+		}
+		return new Gamestate(copy.get(0), copy.get(1), copy.get(2), copy.get(3));
+	}
+
+	void printGamestate() {
+		for (LinkedList<Card> pile : piles) {
+			System.out.println("-----------------------");
+			for (Card c : pile) {
+				c.printCard();
+			}
+		}
+	}
+}
--- Idioten/src/Main.java
+++ Idioten/src/Main.java
@@ -3,44 +3,28 @@ import java.util.*;
 public class Main {
 	
 	public static void main(String args[]){
-//		int failed = 0;
-//		int success = 0;
-//		
-//		
-//
-//		
-//		for(int i = 0; i < 10; i++) {
-//			Deck deck = new Deck();
-//			Gamestate game = new Gamestate(new LinkedList<Card>(), new LinkedList<Card>(), new LinkedList<Card>(), new LinkedList<Card>());
-//		
-//			while(!deck.deck.isEmpty()) {
-//				game.dealCards(deck);
-//				game.compareAndRemove();
-//				game.moveCards();
-//			}
-//			
-//			if(game.piles.get(0).size() == 1 && game.piles.get(1).size() == 1 && game.piles.get(2).size() == 1 &&game.piles.get(3).size() == 1) {
-//				success++;
-//			} else {
-//				failed++;
-//			}
-//			
-//		}
-//		System.out.println("Fails = " + failed);
-//		System.out.println("Success = " + success);
-//		
+		int failed = 0;
+		int success = 0;
+
+		for(int i = 0; i < 1000; i++) {
 			Deck deck = new Deck();
 			Gamestate game = new Gamestate(new LinkedList<Card>(), new LinkedList<Card>(), new LinkedList<Card>(), new LinkedList<Card>());
-			game.dealCards(deck);
-			game.printGamestate();
-			Gamestate copy = game.cloneGamestate();
-			copy.printGamestate();
-			game.dealCards(deck);
-			copy.printGamestate();
-			game.printGamestate();
-//			copy.dealCards(deck);
-//			copy.printGamestate();
-//			game.printGamestate();
+		
+			while(!deck.deck.isEmpty()) {
+				game.dealCards(deck);
+				game.compareAndRemove();
+				game.moveCards();
+			}
+			
+			if(game.piles.get(0).size() == 1 && game.piles.get(1).size() == 1 && game.piles.get(2).size() == 1 &&game.piles.get(3).size() == 1) {
+				success++;
+			} else {
+				failed++;
+			}
+			
+		}
+		System.out.println("Fails = " + failed);
+		System.out.println("Success = " + success);
 		
 	}
 	
